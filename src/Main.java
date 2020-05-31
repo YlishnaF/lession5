09b7662eb50eb6f10550eca122ff8b7ddb84ce11
geetbrains <ylishnaf@yandex.ru>
2020-05-31 19:49:00 +0500

    Дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         final int SIZE = 10000000;
         final int HALF = SIZE / 2;
         float[] arr = new float[SIZE];
@@ -30,19 +32,29 @@ public class Main {
 
         Thread t1 = new Thread(()->{
             for (int i = 0; i < HALF; i++) {
-                a1[i] = arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+
             }
         });
 
         Thread t2 = new Thread(()->{
             for (int i = 0; i < HALF; i++) {
-                a2[i] = arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-
+                a2[i] =  (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
         });
 
         t1.start();
+        try {
+            t1.join();
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         t2.start();
+        try {
+            t2.join();
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         System.arraycopy(a1, 0, arr, 0, HALF);
         System.arraycopy(a2, 0, arr, HALF, HALF);
